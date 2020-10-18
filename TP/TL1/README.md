21d215a54d0529b87d4dd949adf0b3095856f047
Diogo Nunes <up201808546@fe.up.pt>
2020-10-18 18:04:53 +0100

    demonstration instructions

---
+++ TP/TL1/README.md
@@ -0,0 +1,6 @@
+Para usar demonstration.sh:
+
+- Abrir um terminal > Preferências > Criar perfil > Nome: RCOM.
+- Comando > Ao terminar comando: Manter o terminal aberto
+- ./demonstration.sh e colocar password de sudo no terminal que vai aparecer
+- Passados 10seg da instrução acima, 2 terminais abrirão (emissor e recetor)
--- TP/TL1/demonstration.sh
+++ TP/TL1/demonstration.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-gnome-terminal --window-with-profile=RCOM_socat -- sudo ./cable
+gnome-terminal --window-with-profile=RCOM -- sudo ./cable
 
 echo "Sleeping for 10sec waiting for sudo password...";
 sleep 10
@@ -8,8 +8,8 @@ sleep 10
 cd src
 make -s
 if [ $? -eq 0 ] ; then
-  gnome-terminal --window-with-profile=RCOM_recetor -- ./recetor /dev/ttyS10
-  gnome-terminal --window-with-profile=RCOM_emissor -- ./emissor /dev/ttyS11
+  gnome-terminal --window-with-profile=RCOM -- ./recetor /dev/ttyS10
+  gnome-terminal --window-with-profile=RCOM -- ./emissor /dev/ttyS11
 else
   echo "MAKE ERROR";
 fi
