7a8db3c551b23ba6e973f820442dc5a4ab7fa6c8
Diogo Nunes <up201808546@fe.up.pt>
2020-11-02 13:35:12 +0000

    organizing folder

--- TP/TL1/demonstration.sh
+++ TP/TL1/demonstration.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-gnome-terminal --window-with-profile=RCOM -- sudo ./cable
+gnome-terminal --window-with-profile=RCOM -- sudo ./src/cable
 
 echo "Sleeping for 10sec waiting for sudo password...";
 sleep 10
 
-
+cd src
 make -s
 if [ $? -eq 0 ] ; then
   gnome-terminal --window-with-profile=RCOM -- ./app recetor /dev/ttyS11 ../docs/
