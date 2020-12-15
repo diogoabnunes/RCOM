659d9a50dfe4f102865549856ac33d9f590be40c
Diogo Nunes <up201808546@fe.up.pt>
2020-12-15 16:42:49 +0000

    scripts tuxs e gtkterm.txt

--- TL2/scripts/tux43.sh
+++ TL2/scripts/tux43.sh
@@ -3,41 +3,7 @@
 # Configuração de IPs
 ifconfig eth0 up
 ifconfig eth0 172.16.40.1/24
-ifconfig eth0
-
-# Configuração de VLANs (GtkTerm)
-    # configure terminal
-    # vlan 40
-    # end
-
-    # configure terminal
-    # interface fastethernet 0/3 #(0/3 porque o tux43 está ligado na porta 3, depende da ligação dos cabos)
-    # switchport mode access
-    # switchport access vlan 40
-    # end
-
-    # configure terminal
-    # interface fastethernet 0/13 #(0/13 porque o tux44 está ligado na porta 13)
-    # switchport mode access
-    # switchport access vlan 40
-    # end
-
-    # configure terminal
-    # vlan 41
-    # end
-
-    # configure terminal
-    # interface fastethernet 0/1 #(0/1 porque o tux42 está ligado na porta 1)
-    # switchport mode access
-    # switchport access vlan 41
-    # end
-
-# Configuração de um router em Linux
-    # configure terminal
-    # interface fastethernet 0/13
-    # switchport mode access
-    # switchport access vlan 41
-    # end
+ifconfig eth0    
 
 route add -net 172.16.41.0/24 gw 172.16.40.254
 
